a857113030cbd77a0bcba65536c8e3cb430783fd
dongdong <zisehaiyang04@163.com>
2018-08-16 23:30:31 +0800

    dongdong

--- digital_root.py
+++ digital_root.py
@@ -1,17 +1,41 @@
-def digital_root(n):
-    c = 0
-    for i in str(n):
-        #print("i的值是%s"%i)
-        c += int(i)
-    if c <= 9:
-        return c
-    else:
-        a = digital_root(c)
-        #print("c的值是%d"%a)
-        return a
+# def digital_root(n):  # 时间复杂度O(n),空间复杂度O(1),
+#     c = 0
+#     for i in str(n):
+#         #print("i的值是%s"%i)
+#         c += int(i)
+#     if c <= 9:
+#         return c
+#     else:
+#         a = digital_root(c)
+#         #print("c的值是%d"%a)
+#         return a
 
-print(digital_root(16))
-print(digital_root(942))
-print(digital_root(132189))
-print(digital_root(493193))
-print(digital_root(9))
+
+# 优化方案,利用map函数拆分整数，使用递归,时间复杂度O(1),空间复杂度大于O(1)，约等于O(1)
+def digital_root(n, num=1):
+    digital_list = list(map(int, str(n)))
+    digital = sum(digital_list)
+    if digital > 9:
+        num += 1
+        digital = digital_root(digital, num)
+
+    print('num', num)
+    return digital
+
+if __name__ == "__main__":
+    # print(digital_root(16))  # O(1)
+    # print(digital_root(942))  # O(2)
+    # print(digital_root(132189))  # O(2)
+    print(digital_root(493193253564758969098876543456677889987675445678898877656434567))  # O(3)
+    # print(digital_root(9))
+    # ----------------------------------
+
+
+"""
+把整数里的所有数字相加，直到变成个位数，返回
+16-->7
+942-->6
+132189-->6
+493193-->2
+9-->9
+"""
--- equals.py
+++ equals.py
@@ -1,12 +1,22 @@
-def f(n):
-    sum = 0
-    for i in range(n+1):
-        sum += i
+# def f(n):  # 方案一，时间复杂度O(n)
+#     sums = 0
+#     for i in range(n+1):
+#         sums += i
+#
+#     return sums
 
-    return sum
 
+def f(n):  # 方案二  时间复杂度O(1)
+    """
+    # 求n以下（含n）的所有整数和
+    :param n:
+    :return: sum
+    """
+    sums = (n + 1) * n // 2
+    return sums
 
-print(f(1))
-print(f(100))
-        
-print(type(f(1)))
+if __name__ == "__main__":
+    print(f(1))
+    print(f(100))
+
+# print(type(f(1)))
